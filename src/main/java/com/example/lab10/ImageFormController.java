1cf430a05907ab5ad747e09cae88c62e8542c8bc
HotDog1123 <karolpietrow2004@gmail.com>
2024-07-08 20:59:17 +0200

    zad9

--- src/main/java/com/example/lab10/ImageFormController.java
+++ src/main/java/com/example/lab10/ImageFormController.java
@@ -48,51 +48,20 @@ public class ImageFormController {
         }
     }
 
-//    public void incBrightness(BufferedImage image, int factor) {
-//        for (int x = 0; x < image.getHeight(); x++) {
-//            for (int y = 0; y < image.getWidth(); y++) {
-//                int pixel = image.getRGB(x, y);
-//                int mask = 255;
-//                int blue = pixel & mask;
-//                int green = (pixel >> 8) & mask;
-//                int red = (pixel >> 16) & mask;
-//                blue = Math.min(255, blue + factor);
-//                green = Math.min(255, green + factor);
-//                red = Math.min(255, red + factor);
-//                int newPixel = (red << 16) | (green << 8) | blue;
-//                image.setRGB(x, y, newPixel);
-//            }
-//        }
-//    }
-
     public void incBrightness(BufferedImage image, int factor) {
         for (int x = 0; x < image.getHeight(); x++) {
             for (int y = 0; y < image.getWidth(); y++) {
                 int pixel = image.getRGB(x, y);
-                pixel = brightenPixel(pixel, factor);
-                image.setRGB(x, y, pixel);
+                int mask = 255;
+                int blue = pixel & mask;
+                int green = (pixel >> 8) & mask;
+                int red = (pixel >> 16) & mask;
+                blue = Math.min(255, blue + factor);
+                green = Math.min(255, green + factor);
+                red = Math.min(255, red + factor);
+                int newPixel = (red << 16) | (green << 8) | blue;
+                image.setRGB(x, y, newPixel);
             }
-        }
-    }
-
-
-    private int brightenPixel(int pixel, int factor) {
-        int mask = 255;
-        int blue = pixel & mask;
-        int green = (pixel >> 8) & mask;
-        int red = (pixel >> 16) & mask;
-        blue = brightenPixelPart(blue, factor);
-        green = brightenPixelPart(red, factor);
-        red = brightenPixelPart(red, factor);
-        return blue + (green << 8) + (red << 16);
-    }
-
-    private int brightenPixelPart(int color, int factor) {
-        color += factor;
-        if (color > 255) {
-            return 255;
-        } else {
-            return color;
         }
     }
 
